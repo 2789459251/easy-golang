5f05f0d20399e1d8f7b4a87a26bc5f37d24178ca
2789459251 <18291734409@stu.xupt.edu.cn>
2023-11-06 18:13:01 +0800

    单表操作

---
+++ HOOK/init.go
@@ -0,0 +1,47 @@
+package main
+
+import (
+	"fmt"
+	"gorm.io/driver/mysql"
+	"gorm.io/gorm"
+	"gorm.io/gorm/logger"
+	"gorm.io/gorm/schema"
+)
+
+var mysqlLogger logger.Interface
+var DB *gorm.DB
+
+func init() {
+	username := "root"  //账号
+	password := "123"   //密码
+	host := "127.0.0.1" //数据库地址，可以是Ip或者域名
+	port := 3306        //数据库端口
+	Dbname := "gorm"    //数据库名
+	timeout := "10s"    //连接超时，10秒
+
+	mysqlLogger = logger.Default.LogMode(logger.Info) //全局日志===显示日志等级
+	// root:root@tcp(127.0.0.1:3306)/gorm?
+	dsn := fmt.Sprintf("%s:%s@tcp(%s:%d)/%s?charset=utf8mb4&parseTime=True&loc=Local&timeout=%s", username, password, host, port, Dbname, timeout)
+	//连接MYSQL, 获得DB类型实例，用于后面的数据库读写操作。
+	db, err := gorm.Open(mysql.Open(dsn), &gorm.Config{
+		SkipDefaultTransaction: true, //开启跳过默认事物
+		NamingStrategy: schema.NamingStrategy{
+			TablePrefix:   "f_",  // 表名前缀
+			SingularTable: false, // 单数表名，不开启单数
+			NoLowerCase:   false, // 关闭小写转换
+		},
+		//Logger: mysqlLogger,//全局日志
+	})
+	if err != nil {
+		panic("连接数据库失败, error=" + err.Error())
+	}
+	// 连接成功
+	DB = db
+}
+
+//func main() {
+//	//DB = DB.Session(&gorm.Session{
+//	//	Logger: mysqlLogger,
+//	//})
+//	DB.Debug().AutoMigrate(&Student{})
+//}
---
+++ HOOK/单表操作.go
@@ -0,0 +1,92 @@
+package main
+
+import (
+	"gorm.io/gorm"
+)
+
+type Student struct {
+	ID     uint   `gorm:"size:3"`
+	Name   string `gorm:"size:8"`
+	Age    int    `gorm:"size:3"`
+	Gender bool
+	Email  *string `gorm:"size:32"`
+}
+
+func (user *Student) BeforeCreate(tx *gorm.DB) (err error) {
+	email := "test.qq.com"
+	user.Email = &email
+	return nil
+}
+func main() {
+	//DB.AutoMigrate(&Student{})
+	//email := "1826664598@qq.com"
+	//添加
+	//s1 := Student{
+	//	Name:   "小赵爱吃菜",
+	//	Age:    8,
+	//	Gender: true,
+	//	Email:  &email,
+	//}
+	//err := DB.Create(&s1).Error
+	//if err != nil {
+	//	fmt.Println("添加失败")
+	//}
+	//fmt.Println(err)
+	//fmt.Println(s1)
+
+	//批量插入
+	//var studentList []Student
+	//for i := 0; i < 10; i++ {
+	//	studentList = append(studentList, Student{
+	//		Name:   fmt.Sprintf("小赵爱数%d", i+1),
+	//		Age:    9 + i,
+	//		Gender: true,
+	//		Email:  &email,
+	//	})
+	//}
+	//err1 := DB.Create(&studentList).Error
+	//fmt.Println(err1)
+
+	//单条查询
+	//var s1 Student
+	//DB = DB.Session(&gorm.Session{
+	//	Logger: mysqlLogger,
+	//})
+	//DB.First(&s1)
+	//DB.Last(&s1)
+	//DB.Take(&s1, "name = ?", "小赵爱数5")
+	//转意造成的sql注入
+	//DB.Take(&s1, fmt.Sprintf("name = '%s'", "小赵爱吃菜'or 1 = 1#"))
+
+	//s1.ID = 5
+	//DB.Take(&s1)
+	//fmt.Println(s1)
+
+	//查询多条记录
+
+	//var studentList []Student
+	DB = DB.Session(&gorm.Session{
+		Logger: mysqlLogger,
+	})
+	//cnt := DB.Find(&studentList).RowsAffected
+	//fmt.Println(cnt)
+	////for _, student := range studentList {
+	////	fmt.Println(student)
+	////}
+	//data, _ := json.Marshal(studentList)
+	//fmt.Println(string(data))
+
+	//DB.Find(&studentList, []int{4, 7, 9})
+	//fmt.Println(studentList)
+
+	//DB.Find(&studentList, "name in (?)", []string{"小赵爱数6", "小赵爱数8"})
+	//fmt.Println(studentList)
+	//var student Student
+	//DB.Take(&student, "name = ?", "错误")
+	//student.Name = "正确"
+	//DB.Select("name").Save(&student)
+	DB.Create(&Student{
+		Name: "一休哥",
+		Age:  5,
+	})
+}
